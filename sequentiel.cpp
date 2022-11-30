98b8135a4752b4dcba44b7aff87d0bc5f82d924b
ahenches <57415229+ahenches@users.noreply.github.com>
2022-11-30 12:57:02 +0100

    Update
    J'ai fait le code (2) i et ii

--- sequentiel.cpp
+++ sequentiel.cpp
@@ -1,8 +1,6 @@
 #include "src/inverter.h"
 #include "sequentiel.hpp"
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+
 
 
 
@@ -15,33 +13,61 @@ int main(){
     // On utilise le code de @author bjd2385 trouvé à https://gist.github.com/bjd2385/7f4685e703f7437e513608f41c65bbd7
     // j'inverse une matrice 2x2 pour tester voir "matrice 2x2 inversion exemple.png" dans testing
     srand(time(NULL));
+
+    double c0 = 1, c1 = 2, c2 = 3, c3 = 4; 
+
     int mat_size = 2;
 
-    gsl_matrix *mat = gsl_matrix_alloc(mat_size, mat_size);
+    gsl_matrix *b_m_moins_un = gsl_matrix_alloc(mat_size, mat_size);
 
-    // 
-    gsl_matrix_set(mat, 0, 0, 1);
-    gsl_matrix_set(mat, 0, 1, 2);
-    gsl_matrix_set(mat, 1, 0, 3);
-    gsl_matrix_set(mat, 1, 1, 4);
+    gsl_matrix_set(b_m_moins_un, 0, 0, c0);
+    gsl_matrix_set(b_m_moins_un, 0, 1, c1);
+    gsl_matrix_set(b_m_moins_un, 1, 0, c1);
+    gsl_matrix_set(b_m_moins_un, 1, 1, c2);
+
+    gsl_matrix *b_m = gsl_matrix_alloc(mat_size, mat_size);
+
+    gsl_matrix_set(b_m, 0, 0, c1);
+    gsl_matrix_set(b_m, 0, 1, c2);
+    gsl_matrix_set(b_m, 1, 0, c2);
+    gsl_matrix_set(b_m, 1, 1, c3);
 
     // fill this matrix with random doubles
     // randomize_mat_contents(mat, mat_size);
 
     // let's see the original now
-    printf("Original matrix:\n");
-    print_mat_contents(mat, mat_size);
+    printf("Matrice originale b_m_moins_un :\n");
+    print_mat_contents(b_m_moins_un, mat_size);
+
+    
 
     // compute the matrix inverse
-    gsl_matrix *inverse = invert_a_matrix(mat, mat_size);
-    printf("Inverted matrix:\n");
-    print_mat_contents(inverse, mat_size);
+    gsl_matrix *e_m = invert_a_matrix(b_m_moins_un, mat_size);
+    printf("Matrice inversée:\n");
+    print_mat_contents(e_m, mat_size);
 
-    gsl_matrix_free(mat);
-    gsl_matrix_free(inverse);
+    printf("Matrice originale b_m :\n");
+    print_mat_contents(b_m, mat_size);
+
+    gsl_matrix *f_m = gsl_matrix_alloc(mat_size, mat_size);
+
+    gsl_blas_dsymm(CblasLeft, CblasUpper, 1, e_m, b_m, 0, f_m);
+
+    // int gsl_blas_dsymm(CBLAS_SIDE_t Side, CBLAS_UPLO_t Uplo, 1 const gsl_matrix *A, const gsl_matrix *B, 0, gsl_matrix *C)
+
+    printf("Matrice Resultat f_m :\n");
+    print_mat_contents(f_m, mat_size);
 
 
     //Constituer Fm = Em × Bm.
+    // Em est l'inverse d'une matrice symmétrique et est donc aussi symétrique
+    // On multiplie donc deux matrices symétrique
+
+
+    gsl_matrix_free(b_m_moins_un);
+    gsl_matrix_free(e_m);
+    gsl_matrix_free(b_m);
+    gsl_matrix_free(f_m);
     //Calculer les valeurs et vecteurs propres de Fm: (λi, ui) pour i = 1, . . . , m.
 
     //3. Calculer qi = Vm × ui pour i = 1, . . . m
--- sequentiel.hpp
+++ sequentiel.hpp
@@ -1,2 +1,7 @@
 #include <gsl/gsl_matrix_double.h>
 #include <gsl/gsl_linalg.h>
+#include <gsl/gsl_cblas.h>
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
