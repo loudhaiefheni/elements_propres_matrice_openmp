37753486d2b88b1a7a67f52c6e6e504099a07c03
ahenches <arnaud.henches@gmail.com>
2023-01-22 21:07:29 +0100

    Ptit ajstement configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,9 @@
 l=$(find /usr/ -name "gsl")
 # pour les .h (dans le makefile sur la ligne "include=-I(+Resultat de la requète sans gsl)")
 export gsl_include='-I'${l%/*}'/'
+echo "showing variables (source this file with . ./configure) \ngsl_include "$gsl_include
 l=$(find /usr -name "libgsl.a") 
 export gsl_lib='-I'${l%/*}'/'
+echo "gsl_lib "$gsl_lib
 # la librairie pour linker les fichiers compiler (dans le makefile sur la ligne "libs=-L(+Resultat de la requète sans libgsl.a)")
 unset l
