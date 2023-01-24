8c684eca05739c9892bdcfb1c021e3997dd4336d
maudlst <74672202+maudlst@users.noreply.github.com>
2023-01-24 18:43:17 +0100

    Update main.c
    suite algo

--- main.c
+++ main.c
@@ -192,9 +192,13 @@ int main(){
 	gsl_vector *y_k_moins_un = gsl_vector_alloc(n);
 
 	// tableau de vecteurs contenant les vecteur yk  (yk = A*y_(k-1) = A^(k)*y_0 )
-	gsl_vector v_m [m];
-	// tableau de vecteurs contenant les resultats des produits scalaires sur les vecteurs (y_k et y_k-1) ou (y_k et y_k)
+	//gsl_vector v_m [m];
+	gsl_matrix v_m = gsl_matrix_alloc(n,m);
+	// tableau de flottants contenant les resultats des produits scalaires sur les vecteurs (y_k et y_k-1) ou (y_k et y_k)
 	double c[2*m+1];
+
+	//tabelau de vecteur 
+	gsl_vector q[m];
 
 	//matrices contruites a partir du tableau c 
 	gsl_matrix *b_m = gsl_matrix_alloc(m,m);
@@ -205,13 +209,20 @@ int main(){
 	//inverse de la matrice b_m-1
 	gsl_matrix *e_m = gsl_matrix_alloc(m,m);
 
+	//matrice contenant les vecteurs propres et vecteur contenant les valeurs propres
+	gsl_matrix * vecteurs_propres = gsl_matrix_alloc(m, m);
+	gsl_vector * valeurs_propes = gsl_vector_alloc(m);
+
+	//vecteur contentant un vecteur propre
+	gsl_vector * vecteur_propre = gsl_vector_alloc(m);
 
 	
 
 	// FAIRE UNE FONCTION DE TT ça 
 //// Etape 1 
 //1. Calculer C2k−1 = (yk, yk−1), C2k = (yk, yk) où yk = Ayk−1, for k = 1, m.
-	 // WHILE(epsilon_max > precision_souhait
+	 
+	// WHILE(epsilon_max > precision_souhait
 	normalize_vector(x);
 
 	y_k_moins_un = x;
@@ -220,7 +231,7 @@ int main(){
 
 	for(int k = 1 ; k <= m ; k++)
 	{
-		v_m[k-1] = * y_k_moins_un;
+		gsl_matrix_set_col(v_m, k-1, y_k_moins_un)
 		y_k = matrix_vector_product(a, y_k_moins_un);
 		c[2*k-1] = scalar_product(y_k, y_k_moins_un);
 		c[2*k] = scalar_product(y_k, y_k);
@@ -245,23 +256,34 @@ int main(){
 	gsl_blas_dsymm(CblasLeft, CblasUpper, 1, e_m, b_m, 0, f_m);
 
 	// Calcul valeurs et vecteurs propres de f_m
-	gsl_matrix * eigen_vec = gsl_matrix_alloc(n, n);
-	gsl_vector * S = gsl_vector_alloc(n);
 	gsl_eigen_symmv_workspace *my_workspace;
-	my_workspace = gsl_eigen_symmv_alloc(n);
+	my_workspace = gsl_eigen_symmv_alloc(n);/// a voir si c est pa m au lieu de n
 
-	gsl_eigen_symmv(A, S, eigen_vec, my_workspace);
+	gsl_eigen_symmv(f_m, valeurs_propes, vecteurs_propres, my_workspace);
 	gsl_eigen_symmv_free(my_workspace);
-	epsilon_max = 0;
-//	for ( if 
-
 
 //3. Calculer qi = Vm × ui pour i = 1, . . . m
 
+	for(int i = 0; i < m; i++)
+	{
+		gsl_matrix_get_row(vecteur_propre, vecteurs_propres, i);
+		q[i] = matrix_vector_product(v_m, vecteur_propre);
+		printf("Qi = Vm x Ui\n");
+		print_vector_contents(q[i]);
+	}
 
 //4. Si maxi=1,m k(Aqi − λiqi)k ε, alors avec un nouveau vecteur x aller `a l’´etape 1.
 
-
+	est_precis = 1;
+	for (int i =0; i < m ; i++ )
+	{
+		vecteur_A_Qi = matrix_vector_product(a,q[i]);
+		vecteur_lamba_Q = q[i];
+		gsl_vector_scale(vecteur_lamba_Q, gsl_vector_get(valeurs_propres, i));
+		gsl_vector_sub(vecteur_A_Qi, vecteur_lamba_Q);
+		espilon_i = 
+		
+	}
 
 
 
